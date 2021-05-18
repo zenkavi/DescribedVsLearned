48864f6a120daf9970c9cee9380b374d0557611a
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-05-17 17:44:35 -0700

    refactoring and moving onto manipulation checks

---
+++ helpers/clean_behavioral_data.R
@@ -0,0 +1,13 @@
+# Check if data is loaded, load if not
+if (!exists('beh_data')){
+  helpers_path = '~/Dropbox/RangelLab/DescribedVsLearned/helpers/'
+  source(paste0(helpers_path,'get_behavioral_data.R'))
+}
+
+# Clean raw behavioral data
+clean_beh_data = beh_data %>%
+  filter(responded == 1,
+         reactionTime > .2)
+
+rm(beh_data)
+

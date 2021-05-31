f3661635385e4638215721b1d23c1b3bd4324a5e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-05-31 16:15:21 -0700

    refactoring

--- helpers/run_behavioral_analyses.R
+++ helpers/run_behavioral_analyses.R
@@ -5,3 +5,5 @@ source(paste0(helpers_path,'get_behavioral_data.R'))
 source(paste0(helpers_path,'clean_behavioral_data.R'))
 
 source(paste0(helpers_path,'fit_qlearning.R'))
+
+source(paste0(helpers_path,'fit_twoValSystemsWithRL.R'))

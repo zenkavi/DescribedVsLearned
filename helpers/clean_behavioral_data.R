e5417e57c68d6f133da661b919edc102e24f1d44
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-05-31 17:00:18 -0700

    cleaned up first notebook and fixed helpers path

--- helpers/clean_behavioral_data.R
+++ helpers/clean_behavioral_data.R
@@ -1,10 +1,11 @@
+library(here)
+library(tidyverse)
+
 # Check if data is loaded, load if not
 if (!exists('beh_data')){
-  helpers_path = '~/Documents/RangelLab/DescribedVsLearned/helpers/'
+  helpers_path = here('helpers/')
   source(paste0(helpers_path,'get_behavioral_data.R'))
 }
-
-library(tidyverse)
 
 # Clean raw behavioral data
 clean_beh_data = beh_data %>%

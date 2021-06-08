78f0f8e14418fe9932ed7fe54796accedfc12c5c
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-06-08 13:29:37 -0700

    cont working on heuristic

--- bidsify/heuristic.py
+++ bidsify/heuristic.py
@@ -1,37 +1,44 @@
 import os
 
-
 def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
         raise ValueError('Template must be a valid format string')
     return template, outtype, annotation_classes
 
-
 def infotodict(seqinfo):
-    """Heuristic evaluator for determining which runs belong where
-
-    allowed template fields - follow python string module:
-
-    item: index within category
-    subject: participant id
-    seqitem: run number during scanning
-    subindex: sub index within group
-    """
 
     # paths done in BIDS format
     t1w = create_key('sub-{subject}/anat/sub-{subject}_T1w') #anat
     t2w = create_key('sub-{subject}/anat/sub-{subject}_T2w') #anat
-    task = create_key('sub-{subject}/func/sub-{subject}_task-bundles_bold') #func
-    sbref = create_key('sub-{subject}/func/sub-{subject}_task-bundles_sbref') #func
-    fmap_p = create_key('sub-{subject}/func/sub-{subject}_phasediff') #fmap
-    fmap_m = create_key('sub-{subject}/func/sub-{subject}_magnitude') #fmap
-    info = {anat: [], func: [], fmap: []}
+    t2_clinical = create_key('sub-{subject}/anat/sub-{subject}_T2clinical') #anat
+    task = create_key('sub-{subject}/func/sub-{subject}_task-bundles_run-{item:02d}_bold') #func
+    sbref = create_key('sub-{subject}/func/sub-{subject}_task-bundles_run-{item:02d}_sbref') #func
+    fmap_pos = create_key('sub-{subject}/fmap/sub-{subject}_pos') #fmap
+    fmap_neg = create_key('sub-{subject}/fmap/sub-{subject}_neg') #fmap
 
-    data = create_key('run{item:03d}')
-    info = {data: []}
-    last_run = len(seqinfo)
+    info = {t1w: [], t2w: [], task: [], sbref: [], fmap_pos: [], fmap_neg: []}
 
-    for s in seqinfo:
+    for idx, s in enumerate(seqinfo): #each row of dicominfo.tsv
 
-        info[data].append(s.series_id)
+        if (s.TE == 2.56) and ('T1w' in s.protocol_name):
+          info[t1w] = [s.series_id]
+
+        if (s.TE == 206) and ('T2w' in s.protocol_name):
+          info[t2w] = [s.series_id]
+
+        if (s.TE == 90) and ('T2_Clinical' in s.protocol_name):
+           info[t2_clinical] = [s.series_id]
+
+        if (s.dim4 == 892) and ('BOLD_MB' in s.protocol_name):
+          info[task] = [s.series_id]
+
+        if (s.dim4 == 1) and ('BOLD_MB' in s.protocol_name):
+           info[sbref] = [s.series_id]
+
+        if (s.TE == 50) and ('Fieldmap_Pos' in s.protocol_name):
+           info[fmap_pos] = [s.series_id]
+
+        if (s.TE == 50) and ('Fieldmap_Neg' in s.protocol_name):
+           info[fmap_neg] = [s.series_id]
+
     return info

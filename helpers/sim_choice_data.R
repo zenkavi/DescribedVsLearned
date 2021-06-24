b87cac4f4787b0d0691dc541caaf39e1953359cb
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-06-24 16:30:45 -0700

    remove sim output and debug sim

--- helpers/sim_choice_data.R
+++ helpers/sim_choice_data.R
@@ -1,6 +1,8 @@
-sim_choice_data = function(trials, pars){
+sim_choice_data = function(trials, pars, logLik=F, data=NA){
   
-  data = trials
+  if(is.na(data)[1]){
+    data = trials
+  }
   
   a = pars$alpha
   b = pars$beta
@@ -8,33 +10,36 @@ sim_choice_data = function(trials, pars){
   d = pars$delta
   
   # Initialize Q Values
-  data$leftQV = 0
-  data$rightQV = 0
+  leftQV = 0
+  rightQV = 0
   
   for (i in 1:nrow(data)){
     
     if(i < nrow(data)){
-      data$leftQV[i+1] = data$leftQV[i] + a * (data$leftFractalReward[i] - data$leftQV[i])
-      data$rightQV[i+1] = data$rightQV[i] + a * (data$rightFractalReward[i] - data$rightQV[i])
+      leftQV[i+1] = leftQV[i] + a * (leftFractalReward[i] - leftQV[i])
+      rightQV[i+1] = rightQV[i] + a * (rightFractalReward[i] - rightQV[i])
     }
     
   }
   
-  data$leftEV = data$leftLotteryValue * data$leftLotteryProb
-  data$rightEV = data$rightLotteryValue * data$rightLotteryProb
+  leftEV = leftLotteryValue * leftLotteryProb
+  rightEV = rightLotteryValue * rightLotteryProb
 
-  data$wProbFrac = (d * (data$probFractalDraw^g) ) / ( (d * (data$probFractalDraw^g)) + (1-data$probFractalDraw)^g )
+  wProbFrac = (d * (probFractalDraw^g) ) / ( (d * (probFractalDraw^g)) + (1-probFractalDraw)^g )
 
-  data$optValLeft = (1-data$wProbFrac)*data$leftEV + data$wProbFrac*data$leftQV
-  data$optValRight = (1-data$wProbFrac)*data$rightEV + data$wProbFrac*data$rightQV
+  optValLeft = (1-wProbFrac)*leftEV + wProbFrac*leftQV
+  optValRight = (1-wProbFrac)*rightEV + wProbFrac*rightQV
 
   # Inverse logit of value difference weighted by beta
-  x = b * (data$optValLeft - data$optValRight)
+  x = b * (optValLeft - optValRight)
   choice_prob = exp(x)/(1+exp(x))
-  data$choiceLeft = rbinom(nrow(data), 1, choice_prob)
-  data$logLik = dbinom(data$choiceLeft, 1, prob = choice_prob, log=T)
-
-  logLik = sum(data$logLik)
-  out = list(data = data, logLik = logLik)
-  return(out)
+  
+  if(logLik){
+    logLik = dbinom(data$choiceLeft, 1, prob = choice_prob, log=T)
+    logLik = sum(data$logLik)
+    return(logLik)
+  } else{
+    data$choiceLeft = rbinom(nrow(data), 1, choice_prob)
+    return(data)
+  }
 }

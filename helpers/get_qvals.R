7fa9394a0983f828c71531a3840774186260583f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-09-07 13:50:10 -0700

    found bug in q value calculation

--- helpers/get_qvals.R
+++ helpers/get_qvals.R
@@ -6,8 +6,8 @@ get_qvals = function(subj_data){
   for (i in 2:nrow(subj_data)){
     subj_data$leftFractalRpe[i] = subj_data$leftFractalReward[i-1] - subj_data$leftQValue[i-1]
     subj_data$rightFractalRpe[i] = subj_data$rightFractalReward[i-1] - subj_data$rightQValue[i-1]
-    subj_data$leftQValue[i] = subj_data$alpha[i] * (subj_data$leftFractalRpe[i])
-    subj_data$rightQValue[i] = subj_data$alpha[i] * (subj_data$rightFractalRpe[i])
+    subj_data$leftQValue[i] = subj_data$leftQValue[i-1] + subj_data$alpha[i] * (subj_data$leftFractalRpe[i])
+    subj_data$rightQValue[i] = subj_data$rightQValue[i-1] + subj_data$alpha[i] * (subj_data$rightFractalRpe[i])
   }
   return(subj_data)
 }

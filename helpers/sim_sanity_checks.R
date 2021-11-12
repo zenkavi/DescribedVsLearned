97687341ff99997dbc6b77135737370473b3bde9
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-11-11 16:47:38 -0800

    refactoring and making helper functions separate scripts

---
+++ helpers/sim_sanity_checks.R
@@ -0,0 +1,118 @@
+sim_sanity_checks = function(sim_data, checks = c(1,2,3,4,5), compare_rts = TRUE){
+  #Check 1
+  if (1 %in% checks){
+    print(paste0("Proportion of time out trials is: ", round(sum(is.na(sim_data$reactionTime))/nrow(sim_data), 3)))
+  }
+  
+  
+  # Check 2
+  if(2 %in% checks){
+    p = sim_data %>%
+      drop_na()%>%
+      filter(probFractalDraw == 0 | probFractalDraw == 1) %>%
+      mutate(choiceLeft =ifelse(choice == "left", 1, ifelse(choice == "right", 0, NA))) %>%
+      mutate(EVLeftMinRight = EVLeft - EVRight,
+             QVLeftMinRight = QVLeft - QVRight) %>%
+      select(-choice, -EVLeft, -EVRight, -QVLeft, -QVRight) %>%
+      gather(key, value, -probFractalDraw, -reactionTime, -choiceLeft) %>%
+      ggplot(aes(value, choiceLeft))+
+      geom_jitter(height = .1)+
+      geom_smooth(formula = 'y~x', method = "glm", method.args = list(family=binomial), fullrange=TRUE, alpha=.1)+
+      facet_grid(key ~ probFractalDraw)
+    print(p)
+  }
+  
+  
+  # Check 3
+  if(3 %in% checks){
+    if(compare_rts){
+      p = sim_data %>%
+        mutate(data_type = "sim") %>%
+        rbind(sub_data %>%
+                rename(QVLeft = leftQValue, QVRight = rightQValue, EVLeft = leftLotteryEV, EVRight = rightLotteryEV) %>%
+                mutate(choice = ifelse(choiceLeft == 1, "left", "right"),
+                       data_type = "true") %>%
+                select(-subnum, -choiceLeft)) %>%
+        mutate(probFractalDraw = as.factor(probFractalDraw)) %>%
+        ggplot(aes(reactionTime, fill=data_type)) +
+        geom_histogram(position="identity", bins=30, alpha=.5) +
+        labs(title="RT long tail?", fill="")+
+        facet_wrap(~probFractalDraw)+
+        theme(legend.position = "bottom")
+    }
+    else{
+      p = sim_data %>%
+        mutate(probFractalDraw = as.factor(probFractalDraw)) %>%
+        ggplot(aes(reactionTime)) +
+        geom_histogram(position="identity", bins=30) +
+        labs(title="RT long tail?")+
+        facet_wrap(~probFractalDraw)
+    }
+    
+    print(p)
+  }
+  
+  
+  # Check 4
+  if(4 %in% checks){
+    if(compare_rts){
+      p = sim_data %>%
+        mutate(data_type = "sim") %>%
+        rbind(sub_data %>%
+                rename(QVLeft = leftQValue, QVRight = rightQValue, EVLeft = leftLotteryEV, EVRight = rightLotteryEV) %>%
+                mutate(choice = ifelse(choiceLeft == 1, "left", "right"),
+                       data_type = "true") %>%
+                select(-subnum, -choiceLeft)) %>%
+        drop_na()%>%
+        mutate(probFractalDraw = as.factor(probFractalDraw), 
+               log_rt = log(reactionTime)) %>%
+        group_by(probFractalDraw, data_type) %>%
+        summarise(mean_log_rt = mean(log_rt),
+                  sem_log_rt = sem(log_rt), .groups="keep") %>%
+        ggplot(aes(probFractalDraw, mean_log_rt, color=data_type))+
+        geom_point()+
+        geom_errorbar(aes(ymin = mean_log_rt - sem_log_rt, ymax = mean_log_rt + sem_log_rt), width=.2)+
+        labs(title="Inverse U and faster when pFrac ==1?", color="")+
+        theme(legend.position = "bottom")
+    } else{
+      p = sim_data %>%
+        drop_na()%>%
+        mutate(probFractalDraw = as.factor(probFractalDraw), 
+               log_rt = log(reactionTime)) %>%
+        group_by(probFractalDraw) %>%
+        summarise(mean_log_rt = mean(log_rt),
+                  sem_log_rt = sem(log_rt)) %>%
+        ggplot(aes(probFractalDraw, mean_log_rt))+
+        geom_point()+
+        geom_errorbar(aes(ymin = mean_log_rt - sem_log_rt, ymax = mean_log_rt + sem_log_rt), width=.2)+
+        labs(title="Inverse U and faster when pFrac ==1?")
+    }
+    print(p)
+  }
+  
+  # Check 5
+  if(5 %in% checks){
+    p = sim_data %>%
+      mutate(probFractalDraw = as.factor(probFractalDraw),
+             choiceLeft = ifelse(choice == "left", 1, ifelse(choice=="right", 0, NA)),
+             EVDiff = EVLeft - EVRight, 
+             QVDiff = QVLeft - QVRight) %>%
+      nest(data = -probFractalDraw) %>% 
+      mutate(
+        fit = map(data, ~ glm(choiceLeft ~ EVDiff + QVDiff, data = .x, family=binomial(link="logit"))),
+        tidied = map(fit, tidy)
+      ) %>% 
+      unnest(tidied) %>%
+      filter(term != "(Intercept)") %>%
+      select(probFractalDraw, term, estimate, std.error) %>%
+      ggplot(aes(probFractalDraw, estimate, col=term, group=term))+
+      geom_point()+
+      geom_line()+
+      geom_errorbar(aes(ymin = estimate - std.error, ymax = estimate +std.error), width=0.2)+
+      geom_hline(aes(yintercept=0), linetype="dashed")+
+      scale_color_manual(values = cbbPalette[2:1])+
+      theme(legend.position = "bottom")+
+      labs(color="", title="Relevant attribute effect on choice")
+    print(p)
+  }  
+}
---
+++ helpers/sim_task.R
@@ -0,0 +1,32 @@
+sim_task = function(stimuli, ...){
+  kwargs = list(...)
+  if (!("nonDecisionTime" %in% kwargs)){
+    kwargs$nonDecisionTime = 0
+  }
+  if (!("barrierDecay" %in% kwargs)){
+    kwargs$barrierDecay = 0
+  }
+  
+  # Create placeholder output df
+  out = data.frame(EVLeft = NA, EVRight = NA, QVLeft = NA, QVRight = NA, probFractalDraw = NA, choice = NA, reactionTime = NA)
+  
+  # Loop through all trials of a subjects data (or just all the rows of the input)
+  for(i in 1:nrow(sub_data)) {
+    
+    # Simulate RT and choice for a single trial with given DDM parameters and trial stimulus values
+    cur_out = sim_trial(d=kwargs$d, sigma = kwargs$sigma, nonDecisionTime = kwargs$nonDecisionTime, 
+                        barrierDecay = kwargs$barrierDecay,
+                        EVLeft=stimuli$EVLeft[i], EVRight = stimuli$EVRight[i], 
+                        QVLeft = stimuli$QVLeft[i], QVRight= stimuli$QVRight[i] , 
+                        probFractalDraw = stimuli$probFractalDraw[i])
+    
+    # Append the trial to the rest of the output
+    out = rbind(out, cur_out)
+    
+  }
+  
+  # Drops only first row instead of all NAs to keep trials where iterations timed out and a decision was not made
+  out = out[-1,]
+  
+  return(out)
+}

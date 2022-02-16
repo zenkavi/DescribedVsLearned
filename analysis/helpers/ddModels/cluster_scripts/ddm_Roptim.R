222622de68fc8429ea2d1acdcb69646603bd491b
Zeynep Enkavi <zeynepenkavi@gmail.com>
2022-02-16 14:12:50 -0800

    cont working on parallelizing optim

--- analysis/helpers/ddModels/cluster_scripts/ddm_Roptim.R
+++ analysis/helpers/ddModels/cluster_scripts/ddm_Roptim.R
@@ -1,45 +1,61 @@
 #!/usr/bin/env Rscript
 
+library(here)
 library(optparse)
 library(visualMLE)
+
+helpers_path = here('analysis/helpers/')
 source(paste0(helpers_path,'ddmSims/fit_task.R'))
 
+
+#######################
+# Parse input arguments
+#######################
 option_list = list(
-  make_option(c("--data"), type="character", default=NULL),
-  make_option(c("--start_vals"), type="character"),
-  make_option(c("--model_name"), type="character"),
-  make_option(c("--max_iter"), type="integer", default = 500),
-  make_option(c("--par_names"), type="character", default = c("d", "sigma", "delta", "gamma")),
-  make_option(c("--out_path"), type="character")
+  make_option("--data", type="character", default=NULL),
+  make_option("--start_vals", type="character"),
+  make_option("--model", type="character"),
+  make_option("--max_iter", type="integer", default = 500),
+  make_option("--par_names", type="character", default = c("d", "sigma", "delta", "gamma")),
+  make_option("--out_path", type="character")
 ) 
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
+#######################
 # Initialize parameters from input arguments
-
+#######################
 data = read.csv(opt$data)
 
-start_vals = ...
+start_vals = strsplit(opt$start_vals, ",")[[1]]
 
-model_name = opt$model_name
-source(paste0(helpers_path, 'ddmSims/r_ddm_models/', model_name,'.R'))
+model = opt$model
+source(paste0(helpers_path, 'ddmSims/r_ddm_models/', model,'.R'))
 sim_trial_list = list()
 fit_trial_list = list()
-sim_trial_list[[model_name]] = sim_trial
-fit_trial_list[[model_name]] = fit_trial
+sim_trial_list[[model]] = sim_trial
+fit_trial_list[[model]] = fit_trial
 
 max_iter = opt$max_iter
 
-par_names = ...
+par_names = opt$par_names
+if(grepl(',', par_names)){
+  par_names = strsplit(par_names, ',')[[1]]
+}
 
 out_path = opt$out_path
 
+#######################
 # Run optim
-optim_out = optim_save(par = start_vals, get_task_nll, data_=..., par_names_ = par_names, model_name_ = model_name, control = list(maxit=max_iter))
+#######################
+optim_out = optim_save(par = start_vals, get_task_nll, data_= data, par_names_ = par_names, model_name_ = model, control = list(maxit=max_iter))
 
-# Save iterations
-'optim_iter_', start_vals, '_.csv'
+suffix = paste(format(Sys.time(), "%F-%H-%M-%S"), round(runif(1, max=1000)), sep="_")
 
-# Save converged parameters
-'optim_par_', start_vals, '_.csv'
+#######################
+# Save output
+#######################
+write.csv(optim_out$iterations_df, paste0(out_path, 'optim_iter_', model ,'_', suffix,'.csv'), row.names=FALSE)
+write.csv(optim_out$par, paste0(out_path, 'optim_par_', model ,'_', suffix,'.csv'), row.names=FALSE)
+
--- analysis/helpers/ddModels/cluster_scripts/run_ddm_Roptim.sh
+++ analysis/helpers/ddModels/cluster_scripts/run_ddm_Roptim.sh
@@ -1,5 +1,12 @@
 set -e
+while getopts m: flag
+do
+    case "${flag}" in
+        m) model=${OPTARG};;
+    esac
+done
+
 while IFS= read -r line;
 do
-    sed -e "s/{MODELNUM}/model1a/g" -e "s/{START_VALS}/$line/g" run_ddm_Roptim.batch | sbatch
+    sed -e "s/{MODEL}/$model/g" -e "s/{START_VALS}/$line/g" run_ddm_Roptim.batch | sbatch
 done < ddm_Roptim_start_vals.csv

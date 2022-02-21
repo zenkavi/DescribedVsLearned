1d2ae8dae0e9352796379ae07f076a26dae3249d
Zeynep Enkavi <zeynepenkavi@gmail.com>
2022-02-21 14:17:43 -0800

    refactoring to run on different datasets

--- analysis/helpers/ddModels/cluster_scripts/run_ddm_Roptim.sh
+++ analysis/helpers/ddModels/cluster_scripts/run_ddm_Roptim.sh
@@ -11,6 +11,6 @@ done
 while IFS= read -r line;
 do
     sed -e "s/{MODEL}/$model/g" -e "s/{START_VALS}/$line/g" -e "s/{DATA}/$data/g" run_ddm_Roptim.batch | sbatch
-done < $starts
+done < ./start_vals/$starts
 
-# Usage: ./run_ddm_Roptim.sh -m model1a -d sim_single_sub_data -s ddm_optim_start_vals.csv
+# Usage: ./run_ddm_Roptim.sh -m model1a -d sim_single_sub_data1 -s ddm_Roptim_start_vals1.csv

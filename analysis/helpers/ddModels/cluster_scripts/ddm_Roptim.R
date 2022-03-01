b12373e62c29c4380ba1745411e64f2646b7e833
Zeynep Enkavi <zeynepenkavi@gmail.com>
2022-03-01 11:37:30 -0800

    adding parameter fixing option;

--- analysis/helpers/ddModels/cluster_scripts/ddm_Roptim.R
+++ analysis/helpers/ddModels/cluster_scripts/ddm_Roptim.R
@@ -18,6 +18,7 @@ option_list = list(
   make_option("--model", type="character"),
   make_option("--max_iter", type="integer", default = as.integer(500)),
   make_option("--par_names", type="character", default = c("d", "sigma", "delta", "gamma")),
+  make_option("--fix_par_names", type="character"),
   make_option("--out_path", type="character", default = '/ddModels/cluster_scripts/optim_out/')
 ) 
 
@@ -50,13 +51,23 @@ if(length(par_names) == 1){
   }
 }
 
+# Convert fixed parameter info to list format that get_nll > fit_task needs
+fix_par_names = opt$fix_par_names
+if(length(fix_par_names) == 1){
+  if(grepl(',', fix_par_names)){
+    fix_par_names = strsplit(fix_par_names, ', ')[[1]] 
+  }
+}
+fix_par_vals = as.numeric(strsplit(opt$fix_par_vals, ",")[[1]])
+fix_pars = setNames(as.list(fix_par_vals), fix_par_names)
+
 # Must end with /
 out_path = opt$out_path
 
 #######################
 # Run optim
 #######################
-optim_out = optim_save(par = start_vals, get_task_nll, data_= data, par_names_ = par_names, model_name_ = model, control = list(maxit=max_iter))
+optim_out = optim_save(par = start_vals, get_task_nll, data_= data, par_names_ = par_names, model_name_ = model, fix_pars_ = fix_pars, control = list(maxit=max_iter))
 
 suffix = paste(format(Sys.time(), "%F-%H-%M-%S"), round(runif(1, max=1000)), sep="_")
 suffix = paste0(model ,'_', data_suffix, '_', suffix, '.csv')
--- analysis/helpers/ddModels/fit_task.R
+++ analysis/helpers/ddModels/fit_task.R
@@ -10,31 +10,13 @@ my.fit.cluster <- parallel::makeCluster(
   type = "FORK"
 )
 
-#check cluster definition (optional)
-# print(my.cluster)
-
 #register it to be used by %dopar%
 doParallel::registerDoParallel(cl = my.fit.cluster)
 
-#check if it is registered (optional)
-# foreach::getDoParRegistered()
-
-#how many workers are available? (optional)
-# foreach::getDoParWorkers()
-
-
 # Function to fit ddm model to data using a model provided as a string in the model_name argument
-fit_task = function(data_, model_name_, pars_, fit_trial_list_ = fit_trial_list, debug=FALSE){
-  
-  # pars_ = list(...)
-  
-  # Unpack pars_ if pars_ are passed as a list
-  # if(length(pars_) == 1){
-  #   pars_ = pars_[[1]]
-  # }
-  
-  # 
-  # Initialize any missing arguments. Some are useless defaults to make sure different sim_trial functions from different models can run without errors even if they don't make use of that argument
+fit_task = function(data_, model_name_, pars_, fix_pars_ = list(), fit_trial_list_ = fit_trial_list, debug=FALSE){
+   
+  # Initialize any missing arguments. Some are useless defaults to make sure different fit_trial functions from different models can run without errors even if they don't make use of that argument
   if (!("d" %in% names(pars_))){
     pars_$d = 0
   }
@@ -105,6 +87,14 @@ fit_task = function(data_, model_name_, pars_, fit_trial_list_ = fit_trial_list,
   # Extract the correct trial simulator for the model_name
   fit_trial = fit_trial_list_[[model_name_]]
   
+  # Check if there are any pars to be fixed in the fix_pars_ list and replace them in pars_ if there are
+  if(length(fix_pars_) > 0){
+    for(i in 1:length(names(fix_pars_))){
+      cur_fix_par = names(fix_pars_)[i]
+      pars_[[cur_fix_par]] = fix_pars_[[cur_fix_par]]
+    }
+  }
+  
   # Print arguments that will be used for simulation if in debug mode
   if(debug){
     print(paste0("Simulating task with parameters: model_name = ", model_name_,
@@ -133,9 +123,6 @@ fit_task = function(data_, model_name_, pars_, fit_trial_list_ = fit_trial_list,
     ))
   }
   
-  #register it to be used by %dopar%
-  # doParallel::registerDoParallel(cl = my.fit.cluster)
-  
   # Parallel loop
   out <- foreach(
     EVLeft=data_$EVLeft, 
@@ -159,8 +146,6 @@ fit_task = function(data_, model_name_, pars_, fit_trial_list_ = fit_trial_list,
     
   }
   
-  # parallel::stopCluster(cl = my.fit.cluster)
-  
   # Add details of the parameters used for the simulation
   out$model = model_name_
   
@@ -168,17 +153,16 @@ fit_task = function(data_, model_name_, pars_, fit_trial_list_ = fit_trial_list,
 }
 
 # Usage in optim
-# optim(par, get_task_nll, data, par_names, model_name)
-get_task_nll = function(data_, par_, par_names_, model_name_, debug=FALSE){
+# optim(par, get_task_nll, data_, par_names, model_name)
+get_task_nll = function(data_, par_, par_names_, model_name_, fix_pars_){
   
   # Initialize parameters
   # Different models will have different sets of parameters. Optim will optimize over all the parameters it is passed in
-  # There might be a way to use L-BFGS-B and set min's and max's for unused parameters to 0 to avoid optimizing over them but that seems equally bad/worse bc a. you need specify the bounds every time you're fitting anything, b. it won't work with any other algorithm
   
   pars = setNames(as.list(par_), par_names_)
   
   # Get trial likelihoods for the stimuli using the initialized parameters
-  out = fit_task(data_ = data_, model_name_ = model_name_, pars_ = pars, debug=debug)
+  out = fit_task(data_ = data_, model_name_ = model_name_, pars_ = pars, fix_pars_ = fix_pars_)
   
   nll = -sum(log(out$likelihood+1e-200))
   
